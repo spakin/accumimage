66336044539416791767f5fbfe102e143b2932f5
Scott Pakin <scott+gpg@pakin.org>
2025-01-30 21:08:14 -0700

    Document accumcolor.AccumLabA in the package header

--- accumcolor/doc.go
+++ accumcolor/doc.go
@@ -7,5 +7,10 @@ colors together.  It maintains a tally of the total number of colors that have
 been accumulated.  The Add method accumulates more color onto an existing
 AccumNRGBA.  The NRGBA method returns the average color of the entire
 accumulation as a color.NRGBA.
+
+An AccumLabA provides similar functionality to AccumNRGBA but stores colors
+in CIE L*a*b* + alpha channels.  AccumLabA thereby supports a more
+perceptually uniform color space and more natural results when averaging
+colors.
 */
 package accumcolor
